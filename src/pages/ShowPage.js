1b0f016913041befd016eb9d3b1c2202e401ab8e
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-05-10 20:08:26 +0200

    shows changed api

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,29 +1,36 @@
-import { useParams } from "react-router-dom"
-import { getShowByID } from "../helpers/showsHelper"
-import NotFound from "./NotFound"
+import { useParams } from "react-router-dom";
+import { getShowByID } from "../helpers/showsHelper";
+import NotFound from "./NotFound";
+import { useEffect, useState } from "react";
 
 function ShowPage() {
-  const params = useParams()
-  const show = getShowByID(params.showID)
+  const { showID } = useParams();
+  const [show, setShow] = useState(undefined);
 
+  useEffect(() => {
+    const fetchShow = async () => {
+      const showData = await getShowByID(showID);
+      setShow(showData);
+    };
+    fetchShow();
+  }, [showID]);
+
+  if (!show) return <NotFound />;
 
   return (
     <>
-      { 
-        !!show ?  
+      {!!show ? (
         <div className="ShowPage">
           <h1>{show.name}</h1>
           <img src={show.image.medium} />
           <h2>Language: {show.language}</h2>
           <h3>Rating: {show.rating.average}</h3>
-          <h3>Genres: {show.genres.join(', ')}</h3>
+          <h3>Genres: {show.genres.join(", ")}</h3>
         </div>
-        :
+      ) : (
         <NotFound />
-      }
+      )}
     </>
-  )
+  );
 }
-export default ShowPage
-
-
+export default ShowPage;
